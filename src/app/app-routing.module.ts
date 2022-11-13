ca757207d918c8bd0552659dff359830e966fb35
codein-artout <codein.artout@gmail.com>
2022-11-13 20:19:45 +0530

    add modules, routers and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'prefix',
+    redirectTo: 'for-users'
+  },
+  {
+    path: 'for-users',
+    loadChildren: () => import(`src/app/pages/user-view/user-view.module`).then(m => m.UserViewModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
